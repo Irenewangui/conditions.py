a035d37169212b01b2d782be69784591475943b1
rennyvick <irenevky85@gmail.com>
2018-06-14 09:51:21 +0300

    first commit

---
+++ device.py
@@ -0,0 +1,6 @@
+value_1=input("Enter a value")
+value_2=input("Enter a value")
+if value_1>value_2:
+  print("Value 1 is greater")
+else:
+  print("Value 2 is greater")
---
+++ value.py
@@ -0,0 +1,5 @@
+value_1=input("Enter a value")
+value_2=input("Enter a value")
+if value_1>value_2:
+  print("Value 1 is greater")
+else:
